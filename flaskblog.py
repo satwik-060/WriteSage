0f6c36f13932bbf3f417336dc3d8cfcafbf8cef6
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-05-30 11:50:07 +0530

    played with database

--- flaskblog.py
+++ flaskblog.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1f114f296041cf407f10120252bad889'
+app.app_context().push()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
