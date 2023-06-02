5dc9b4f4076991ceb4e32bb442ac030b9d79c61f
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-06-02 22:49:41 +0530

    user login system

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.app_context().push()
 app.config['SECRET_KEY'] = '1f114f296041cf407f10120252bad889'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,8 @@
 '''Forms'''
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
+from wtforms.validators import DataRequired,Length,Email,EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):  
     """Registration Form"""
@@ -11,7 +12,17 @@ class RegistrationForm(FlaskForm):
     password = StringField('Password' , validators = [DataRequired()])
     confirm_password = StringField('Confirm Password',validators = [DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already in use.')
 
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("An account is already created with this Email. Please create another email account to create an account here.")
+        
 class LoginForm(FlaskForm):
     """Login Form"""
     email = StringField('Email', validators = [DataRequired(),Email()])
