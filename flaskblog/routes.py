72a25bebe9e9b88a95785968ec0b4ec69b1e0e93
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-06-04 16:21:37 +0530

    added pagination for home page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -6,11 +6,13 @@ from flask_login import login_user, current_user,logout_user, login_required
 import secrets
 import os
 from PIL import Image
+
 @app.route("/")
 @app.route("/home")
 def home():
     '''Home page'''
-    posts = Post.query.all()
+    page = request.args.get('page',1,type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page , per_page = 5)
     print(posts)
     return render_template('home.html', posts = posts)
 
@@ -140,3 +142,11 @@ def delete_post(post_id):
     db.session.commit()
     flash("your Post has been deleted!",'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    '''User page'''
+    page = request.args.get('page',1,type=int)
+    user = User.query.filter_by(username = username).first_or_404()
+    posts = Post.query.filter_by(author = user).order_by(Post.date_posted.desc()).paginate(page = page , per_page = 5)
+    return render_template('user_post.html',user = user, posts = posts)
