43ab206deae708e13131e11da1a12e00aafa01c9
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-06-06 23:13:24 +0530

    added password reset by sending mail feature

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.app_context().push()
@@ -17,6 +19,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'perisetla.satwik176@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ewqpdyjbvmnlkwig'
+mail = Mail(app)
 
 from flaskblog import routes
 
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 '''Forms'''
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField,SubmitField,BooleanField,TextAreaField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
 from wtforms.validators import DataRequired,Length,Email,EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
@@ -11,8 +11,8 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username' ,validators=[DataRequired(),
                                        Length(min = 2 , max = 20)])
     email = StringField('Email', validators = [DataRequired(),Email()])
-    password = StringField('Password' , validators = [DataRequired()])
-    confirm_password = StringField('Confirm Password',validators = [DataRequired(),EqualTo('password')])
+    password = PasswordField('Password' , validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators = [DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
     
     def validate_username(self, username):
@@ -28,7 +28,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     """Login Form"""
     email = StringField('Email', validators = [DataRequired(),Email()])
-    password = StringField('Password' , validators = [DataRequired()])
+    password = PasswordField('Password' , validators = [DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
@@ -59,3 +59,17 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators = [DataRequired()])
     content = TextAreaField('Content',validators = [DataRequired()])
     submit = SubmitField('Post')
+    
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(),Email()])
+    submit = SubmitField('Request Password Reset')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError("There is no user with that email.")
+
+class ResetPasswordForm(FlaskForm):
+    
+    password = PasswordField('Password' , validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators = [DataRequired(),EqualTo('password')])
+    submit = SubmitField('Reset Password')
