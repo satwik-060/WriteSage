96ad15da91e93c8b73c21ba27140f982889dbffa
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-06-16 16:08:25 +0530

    added requirements.txt

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
 from flask_ckeditor import CKEditor
-from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
