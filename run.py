55bc82dba87dc64112814798ab79d79cd55ae6a6
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-12-18 12:21:53 +0530

    added about page

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 '''Learning Flask'''
 from writesage import create_app
+
+import collections 
+import sys
+if sys.version_info.major == 3 and sys.version_info.minor >= 10:
+    from collections.abc import MutableSet
+    collections.MutableSet = collections.abc.MutableSet
+else: 
+    from collections import MutableSet
+    
 app = create_app()
 
 if __name__ == '__main__':
